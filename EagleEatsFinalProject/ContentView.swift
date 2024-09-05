3e2b946a5955fde4c56f0a54977a2f2945e2d636
Hickmaha <Hickmaha@bc.edu>
2024-09-04 20:45:48 -0400

    Initial Commit

---
+++ EagleEatsFinalProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EagleEatsFinalProject
+//
+//  Created by Halen Hickman-Goveia on 9/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EagleEatsFinalProject/EagleEatsFinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EagleEatsFinalProjectApp.swift
+//  EagleEatsFinalProject
+//
+//  Created by Halen Hickman-Goveia on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct EagleEatsFinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
