684196ca8a01894bc7da65f7ea0cb71ca7352506
Hickmaha <Hickmaha@bc.edu>
2024-09-09 17:57:27 -0400

    Some Preliminary Edits

---
+++ EagleEatsFinalProject/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  EagleEatsFinalProject
+//
+//  Created by Halen Hickman-Goveia on 9/9/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
